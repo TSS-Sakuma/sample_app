fa3611997b675815f79ce9296a7e88ca0e7a11df
TSS-Sakuma <i-sakuma@tss-ipnet.co.jp>
2017-04-20 15:36:45 +0900

    Add user following

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -11,6 +11,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
+#    assert_select 'input[type=FILL_IN]'
     # 無効な送信
     assert_no_difference 'Micropost.count' do
       post microposts_path, micropost: { content: "" }
@@ -18,9 +19,12 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     # 有効な送信
     content = "This micropost really ties the room together"
+#    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, micropost: { content: content }
+      post microposts_path, micropost: { content: content}#,
+      #picture: FILL_IN }
     end
+#    assert FILL_IN.picture?
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
@@ -34,4 +38,19 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     get user_path(users(:archer))
     assert_select 'a', text: 'delete', count: 0
   end
+
+  #test "micropost sidebar count" do
+  #  log_in_as(@user)
+  #  get root_path
+  #  assert_match "#{current_user.microposts.count} microposts", response.body
+    # まだマイクロポストを投稿していないユーザー
+  #  other_user = users(:mallory)
+  #  log_in_as(other_user)
+  #  get root_path
+  #  assert_match "0 microposts", response.body
+  #  other_user.microposts.create!(content: "A micropost")
+  #  get root_path
+  #  assert_match microposts.content, response.body
+  #end
+
 end
